de02eb52ca0242f545aff9861a56151bbc0c6644
yu <2020238020@qq.com>
2026-05-27 15:48:51 +0800

    feat(B模块): 生成商品模块 Mapper 接口

---
+++ mall-backend/src/main/java/com/mall/mapper/CategoryMapper.java
@@ -0,0 +1,13 @@
+package com.mall.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mall.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商品分类Mapper
+ */
+@Mapper
+public interface CategoryMapper extends BaseMapper<Category> {
+}
+
---
+++ mall-backend/src/main/java/com/mall/mapper/NoticeMapper.java
@@ -0,0 +1,13 @@
+package com.mall.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mall.entity.Notice;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 公告Mapper
+ */
+@Mapper
+public interface NoticeMapper extends BaseMapper<Notice> {
+}
+
