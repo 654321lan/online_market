3b38028e70a0e587efe3638d00932d281c073c10
wez798 <2737623977@qq.com>
2026-05-31 19:10:08 +0800

    Create request.js

---
+++ mall-frontend/src/utils/request.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+
+const request = axios.create({
+  baseURL: '/api',
+  timeout: 10000
+})
+
+// 请求拦截器
+request.interceptors.request.use(
+  config => {
+    const userInfo = localStorage.getItem('userInfo')
+    const merchantInfo = localStorage.getItem('merchantInfo')
+    const adminInfo = localStorage.getItem('adminInfo')
+    
+    if (userInfo) {
+      const user = JSON.parse(userInfo)
+      config.headers['userId'] = user.id
+    } else if (merchantInfo) {
+      const merchant = JSON.parse(merchantInfo)
+      config.headers['merchantId'] = merchant.id
+    } else if (adminInfo) {
+      const admin = JSON.parse(adminInfo)
+      config.headers['adminId'] = admin.id
+    }
+    
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器
+request.interceptors.response.use(
+  response => {
+    const res = response.data
+    if (res.code !== 200) {
+      ElMessage.error(res.message || '请求失败')
+      return Promise.reject(new Error(res.message || '请求失败'))
+    }
+    return res
+  },
+  error => {
+    ElMessage.error(error.message || '网络错误')
+    return Promise.reject(error)
+  }
+)
+
+export default request
+
