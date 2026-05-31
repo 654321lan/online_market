1a168f5353fdcc8d5f5a50c8e1ade684c0a51ca4
Ma-yi-arch <2947797114@qq.com>
2026-05-31 18:29:56 +0800

    Add files via upload

---
+++ mall-backend/src/main/java/com/mall/entity/Admin.java
@@ -0,0 +1,32 @@
+package com.mall.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 管理员实体
+ */
+@Data
+@TableName("tb_admin")
+public class Admin {
+    
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    
+    private String username;
+    
+    private String password;
+    
+    private String nickname;
+    
+    private Integer status;
+    
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+    
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+}
+
